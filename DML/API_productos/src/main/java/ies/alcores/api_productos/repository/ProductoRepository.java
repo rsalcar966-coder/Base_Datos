1f14ef37d633f83c86f465e798e4f648aa8aa7e0
rsalcar966-coder <rsalcar966@gmail.com>
2026-02-23 08:55:06 +0100

    Remove API_Producto and DML/API_productos files
    Remove the Angular 'API_Producto' project and the Java 'DML/API_productos' project by deleting their source, config, build, test and IDE files (including package.json, angular.json, tsconfig, pom, src trees, etc.). Add only DML/API_productos/.idea/workspace.xml. This cleans up the repository by removing those projects while preserving the IDE workspace file.

--- DML/API_productos/src/main/java/ies/alcores/api_productos/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package ies.alcores.api_productos.repository;
-
-import ies.alcores.api_productos.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, String> {
-}
